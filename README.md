7ffc26ffd15d0f8b11669b37df6ef86f19a40d37
ThereforeGames <95403634+ThereforeGames@users.noreply.github.com>
2023-11-15 21:28:30 -0500

    v0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# echoproof
- Simple extension for text-generation-webui that injects recent conversation into the negative prompt in order to reduce OCD-like behavior of the assistant fixating on a single word, phrase, or sentence structure.
+# Echoproof
+
+Echoproof is a simple extension for Ooobabooga's [text-generation-webui](https://github.com/oobabooga/text-generation-webui) that injects recent conversation history into the negative prompt with the goal of minimizing the LLM's tendency to fixate on a single word, phrase, or sentence structure.
+
+## The problem
+
+I have observed that certain tokens will cause LLMs to exhibit an "OCD-like" behavior where future messages become progressively more repetitive. If you are not familiar with this effect, try appending a bunch of emoji 👀😲😔 to a chatbot's reply or forcing it to write in ALL CAPS - it will become a broken record very quickly.
+
+This is certainly true of quantized Llama 2 models in the 7b to 30b paramater range - I'm guessing it's less prevalent in 70b models, but I don't have the hardware to test that.
+
+Existing solutions to address this problem, such as `repitition_penalty`, have shown limited success.
+
+This issue can derail a conversation well before the context window is exhausted, so I believe it is unrelated to another known phenomenon where a model will descend into a "word salad" state once the chat has gone on for too long.
+
+## The solution (?)
+
+What if we just inject the last thing the chatbot said into the negative prompt for its next message? That was the main idea behind Echoproof, and it seems to work pretty well.
+
+After testing this approach for a few weeks, I have refined it with a few additional controls:
+
+- **Last Message Multiplier**: The number of times to add the most recent message into the negative prompt. I have found that 1 is not strong enough to offset the OCD effect, but 3-5 makes a noticeable difference.
+- **History Multiplier**: The number of times to add your entire chat history into the negative prompt. If you enable Echoproof from the beginning of a conversation, this feature is probably overkill. However, it might be able to save a conversation that is already starting to go off the rails.
+- **History Message Limit**: Caps the aforementioned feature to the last x messages.
+
+Some models are more prone to repitition than others, so you may need to experiment with these settings to find the right balance.
+
+## How to install
+
+Paste the URL of this repo into the "Session" tab of the webui and press enter. Go get a cup of coffee.
+
+## How to use
+
+Load a model with `cfg-cache` enabled and set your `guidance_scale` to a value above 1 in the "Parameters" tab. Otherwise, your negative prompt will not have an effect.
---
+++ echoproof/script.py
@@ -0,0 +1,89 @@
+"""
+Simple extension that injects recent conversation into the negative prompt in order to reduce OCD-like behavior of the assistant fixating on a single word, phrase, or sentence structure.
+"""
+
+import gradio as gr
+import torch
+from transformers import LogitsProcessor
+
+from modules import chat, shared
+from modules.text_generation import (
+	decode,
+	encode,
+	generate_reply,
+)
+
+params = {
+	"display_name": "Echoproof",
+	"is_tab": False,
+	"debug": False,
+	"delimiter":" ",
+	"history_multiplier":0,
+	"last_msg_multiplier":5,
+	"message_limit":0
+}
+
+VERSION = "0.0.1"
+
+def state_modifier(state):
+	"""
+	Modifies the state variable, which is a dictionary containing the input
+	values in the UI like sliders and checkboxes.
+	"""
+
+	if params["enable"]:
+		internal_history = state["history"]["internal"]
+		history_length = len(internal_history)
+		if params["message_limit"]:
+			_min = max(0,history_length - params["message_limit"])
+		else:
+			_min = 0
+
+		extra_neg = ""
+		for idx in range(_min,len(internal_history)):
+			msg = internal_history[idx]
+			extra_neg += msg[1] + params["delimiter"]
+
+		extra_neg *= params["history_multiplier"]
+
+		if params["last_msg_multiplier"] > 1:
+			extra_neg += state["history"]["internal"][-1][1] * (params["last_msg_multiplier"] - 1)
+
+		if params["debug"]:
+			print(f"Value of `extra_neg`: {extra_neg}")
+		
+		state["negative_prompt"] += extra_neg
+
+	return state
+
+def ui():
+	"""
+	Gets executed when the UI is drawn. Custom gradio elements and
+	their corresponding event handlers should be defined here.
+
+	To learn about gradio components, check out the docs:
+	https://gradio.app/docs/
+	"""
+	with gr.Accordion(f"Echoproof v{VERSION}", open=False):
+
+		gr.Markdown("**Note:** You must load a model with `cfg-cache` enabled and set `guidance_scale` to a value > 1 for Echoproof to take effect.")
+
+		enable = gr.Checkbox(value=True,label="Enable")
+		enable.change(lambda x: params.update({"enable": x}), enable, None)
+
+		debug = gr.Checkbox(value=False,label="Debug")
+		debug.change(lambda x: params.update({"debug": x}), debug, None)
+
+		history_multiplier = gr.Slider(0, 50, label="History Multiplier", info="Adds the conversation history to your negative prompt x times.", value=0, step=1)
+		history_multiplier.change(lambda x: params.update({"history_multiplier": x}), history_multiplier, None)
+
+		last_msg_multiplier = gr.Slider(1, 50, label="Last Message Multiplier", info="Adds the most recent message to your negative prompt x times.", value=5, step=1)
+		last_msg_multiplier.change(lambda x: params.update({"last_msg_multiplier": x}), last_msg_multiplier, None)
+
+		message_limit = gr.Number(value=10,label="History Message Limit")
+		message_limit.change(lambda x: params.update({"message_limit":x}), message_limit, None)
+
+		delimiter = gr.Textbox(value=" ",label="Message Delimiter",info="String that separates each message in the negative prompt.")
+		delimiter.change(lambda x: params.update({"delimiter": x}), delimiter, None)
+
+		gr.Markdown("If you find this project useful, consider [supporting my work here](https://github.com/sponsors/ThereforeGames). Thank you. ❤️")
